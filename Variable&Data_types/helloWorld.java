c512f42cb8ee44d39495b4e4d4b9292e7cd76152
ItsTushar16 <itstiwaribhaiya16@gmail.com>
2026-05-10 14:06:35 +0530

    A

---
+++ Variable&Data_types/helloWorld.java
@@ -0,0 +1,9 @@
+public class helloWorld {
+    public static void main( String args []){
+        System.out.print("Hello world");
+        System.out.println("Life is Enoyyy");
+        System.out.print("Java is fun\n");
+        System.out.println("I am learning Java");
+        System.out.print("* * *");
+    }
+}
