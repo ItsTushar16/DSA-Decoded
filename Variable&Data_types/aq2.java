33c6f2ee0f5adfaee16585b3af4c4a22f6abe923
ItsTushar16 <itstiwaribhaiya16@gmail.com>
2026-05-12 10:55:05 +0530

    12-05-2026 Operators Done

--- Variable&Data_types/aq2.java
+++ Variable&Data_types/aq2.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class aq2{
-    public static void main( String args []){
+    public static void main( String [] args ){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter value of pencil :");
         float pencil = sc.nextFloat();
